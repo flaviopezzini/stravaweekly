b09ea743b3fe4e17044520c003afa101e0f49b2e
Flavio Pezzini <flaviopezzini@gmail.com>
2020-03-29 10:47:37 +0200

    Passing in the start and end dates since Heroku's server has an incorrect time set. I've also added changes to allow the user to browse past weeks, one at a time. I've also removed the button Generate Table and it's now automatically generating the table upon page load.

--- src/main/java/stravaweekly/StravaWeeklyApplication.java
+++ src/main/java/stravaweekly/StravaWeeklyApplication.java
@@ -3,12 +3,9 @@ package stravaweekly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-
-import java.time.Clock;
 
 @SpringBootApplication
 @EnableOAuth2Sso
--- src/main/java/stravaweekly/activity/ActivityController.java
+++ src/main/java/stravaweekly/activity/ActivityController.java
@@ -1,7 +1,6 @@
 package stravaweekly.activity;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +14,10 @@ import java.time.*;
 @RestController
 public class ActivityController {
 
-  private static final Logger logger = LoggerFactory.getLogger(ActivityController.class);
-
   private final RequestService requestService;
-  private final ActivityService activityService;
 
-  public ActivityController(RequestService requestService, ActivityService activityService) {
+  public ActivityController(RequestService requestService) {
     this.requestService = requestService;
-    this.activityService = activityService;
   }
 
   @GetMapping("/athlete/clubs")
@@ -35,20 +30,18 @@ public class ActivityController {
   @GetMapping("/athlete/activities")
   public ResponseEntity<String> listAthleteActivities(
           final @AuthenticationPrincipal Principal principal,
-          @RequestParam(name = "userTimezoneOffsetInHours", required = true) int userTimezoneOffsetInHours) {
-
-    logger.info("userTimezoneOffsetInHours = " + userTimezoneOffsetInHours);
+          @RequestParam(name = "userTimezoneOffsetInHours") int userTimezoneOffsetInHours,
+          @RequestParam(name = "startDate")
+          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                  LocalDateTime startDate,
+          @RequestParam(name = "endDate")
+          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                  LocalDateTime endDate) {
 
     // do all calculations on dates based on the user timezone
     ZoneId userZoneId = ZoneId.ofOffset("", ZoneOffset.ofHours(userTimezoneOffsetInHours));
-
-    logger.info("userZoneId = " + userZoneId);
-
-    ZonedDateTime zonedEnd = activityService.getEndDate(userZoneId);
-    ZonedDateTime zonedStart = activityService.getStartDate(zonedEnd);
-
-    logger.info("zonedEnd = " + zonedEnd);
-    logger.info("zonedStart = " + zonedStart);
+    ZonedDateTime zonedStart = ZonedDateTime.of(startDate, userZoneId);
+    ZonedDateTime zonedEnd = ZonedDateTime.of(endDate, userZoneId);
 
     final String url = String.format("https://www.strava.com/api/v3/athlete/activities?before=%s&after=%s",
             (zonedEnd.toInstant().toEpochMilli() / 1000),

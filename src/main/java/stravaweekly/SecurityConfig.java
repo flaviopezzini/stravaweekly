b8975d2027bc321f5facf48000d8f5a6004ce353
Flavio Pezzini <flavio.pezzini@toptal.com>
2023-06-17 09:41:56 +0100

    Added initial work to try and get a refresh token flow working.

--- src/main/java/stravaweekly/SecurityConfig.java
+++ src/main/java/stravaweekly/SecurityConfig.java
@@ -3,6 +3,12 @@ package stravaweekly;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -38,4 +44,25 @@ public class SecurityConfig implements WebMvcConfigurer {
             //.loginPage("/index.html");
     return http.build();
   }
+
+@Bean
+public OAuth2AuthorizedClientManager authorizedClientManager(
+		ClientRegistrationRepository clientRegistrationRepository,
+		OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+	  OAuth2AuthorizedClientProvider authorizedClientProvider =
+			OAuth2AuthorizedClientProviderBuilder.builder()
+					.authorizationCode()
+					.refreshToken()
+					.clientCredentials()
+					.password()
+					.build();
+
+	  DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+			new DefaultOAuth2AuthorizedClientManager(
+					clientRegistrationRepository, authorizedClientRepository);
+	  authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+	  return authorizedClientManager;
+  }  
 }
